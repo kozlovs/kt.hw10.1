f11489c5eab65d5a197c299b940b802e25da4def
Sergey Kozlov <kozlov_s_s@outlook.com>
2022-06-25 23:34:56 +1000

    Chat commit 3

--- src/test/kotlin/post/WallServiceTest.kt
+++ src/test/kotlin/post/WallServiceTest.kt
@@ -33,14 +33,14 @@ class WallServiceTest {
         val post = WallService.add(Post(0L, 0L, 0L, 0L, 0L, 0L))
         val comment = Comment(0, post.id, 0, "some thread", 0, null)
         val returnComment = WallService.createComment(post.id, comment)
-        assertEquals(comment, returnComment)
+        assertTrue(returnComment in post.comments)
     }
 
     @Test(expected = PostNotFoundException::class)
     fun commentShouldThrow() {
         val post = WallService.add(Post(0L, 0L, 0L, 0L, 0L, 0L))
         val falsePostId = post.id + 1
-        val comment = Comment(0, falsePostId,0, "some thread", 0, null)
+        val comment = Comment(0, falsePostId, 0, "some thread", 0, null)
         WallService.createComment(falsePostId, comment)
     }
 
@@ -48,19 +48,16 @@ class WallServiceTest {
     fun reportShouldNotThrow() {
         val post = WallService.add(Post(0L, 0L, 0L, 0L, 0L, 0L))
         val comment = Comment(0, post.id, 0, "some thread", 0, null)
-        WallService.createComment(post.id, comment)
-        val report = ReportComment(0, comment.id, 1)
-        val returnReport = WallService.createReport(post.id, comment.id, report)
-        assertEquals(
-            report,
-            returnReport
-        )
+        val newComment = WallService.createComment(post.id, comment)
+        val report = ReportComment(0, newComment.id, 1)
+        val returnReport = WallService.createReport(post.id, newComment.id, report)
+        assertEquals(report, returnReport)
     }
 
     @Test(expected = UnknownReasonException::class)
     fun reportShouldThrowUnknownReasonException() {
         val post = WallService.add(Post(0L, 0L, 0L, 0L, 0L, 0L))
-        val comment = Comment(0, post.id,0, "some thread", 0, null)
+        val comment = Comment(0, post.id, 0, "some thread", 0, null)
         WallService.createComment(post.id, comment)
         val report = ReportComment(0, comment.id, -1)
         WallService.createReport(post.id, comment.id, report)
